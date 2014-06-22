5800262f6be2b6141cbd6ad980b02507904f8613
masta-g3 <manolorueda@gmail.com>
2014-06-22 17:32:08 -0500

    First pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions will take a given matrix, calculate the inverse
+## an store it on a new object. If the original matrix remains the same,
+## the second function will read the inverse matrix from the cache,
+## avoiding the calculation time of recomputing it again.
 
-## Write a short comment describing this function
+## First function: "makeCacheMatrix.R". Compromised of a list of 4 functions
+## for getting and setting the values of the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
-
 
 ## Write a short comment describing this function
 
